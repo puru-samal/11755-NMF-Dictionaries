9ff4353e1e2477f30b09cc14aaabce571605eecf
Puru Samal <puruboii@gmail.com>
2024-12-13 10:07:59 -0500

    added mode...

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import yaml
 import torch
 import os
 import argparse
+from typing import Literal
 from dataset import MUSDBDataset
 from torch.utils.data import DataLoader
 from utils import batch_encode, batch_decode
 from nmf import train_nmf_dictionary, test_separation
 from datetime import datetime
 
-def main(config_path):
+def main(config_path, mode:Literal['train', 'test']='train'):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     DEBUG = False   # Set to True to print debug information
@@ -94,75 +95,80 @@ def main(config_path):
     ## --------------------------------------------------------------------------   
 
     ## Train Target NMF dictionary ----------------------------------------------
-    print("Training target NMF dictionary...")
-    B_target = train_nmf_dictionary(
-        dataloader  = train_dataloader,
-        device      = device,
-        train_target= 'target',
-        K           = config['nmf']['target_K'], 
-        n_iter      = config['nmf']['n_iter'], 
-        tol         = config['nmf']['tol'], 
-        beta        = config['nmf']['beta'],
-        alpha       = config['nmf']['alpha'],
-    )
-    print("Target NMF dictionary trained\n")
+    if mode == 'train':
+        print("Training target NMF dictionary...")
+        B_target = train_nmf_dictionary(
+            dataloader  = train_dataloader,
+            device      = device,
+            train_target= 'target',
+            K           = config['nmf']['target_K'], 
+            n_iter      = config['nmf']['n_iter'], 
+            tol         = config['nmf']['tol'], 
+            beta        = config['nmf']['beta'],
+            alpha       = config['nmf']['alpha'],
+        )
+        print("Target NMF dictionary trained\n")
     ## --------------------------------------------------------------------------
 
     ## Train Background NMF dictionary -------------------------------------------
-    print("Training background NMF dictionary...")
-    B_background = train_nmf_dictionary(
-        dataloader  = train_dataloader,
-        device      = device,
-        train_target= 'background',
-        K           = config['nmf']['background_K'], 
-        n_iter      = config['nmf']['n_iter'], 
-        tol         = config['nmf']['tol'], 
-        beta        = config['nmf']['beta'],
-        alpha       = config['nmf']['alpha'],
-    )
-    print("Background NMF dictionary trained\n")
+        print("Training background NMF dictionary...")
+        B_background = train_nmf_dictionary(
+            dataloader  = train_dataloader,
+            device      = device,
+            train_target= 'background',
+            K           = config['nmf']['background_K'], 
+            n_iter      = config['nmf']['n_iter'], 
+            tol         = config['nmf']['tol'], 
+            beta        = config['nmf']['beta'],
+            alpha       = config['nmf']['alpha'],
+        )
+        print("Background NMF dictionary trained\n")
     ## --------------------------------------------------------------------------   
 
     ## Save dictionary ----------------------------------------------------------
     # save to results directory
-    torch.save(B_target, f'results/{id}/{config["dataset"]["target"]}_nmf_dictionary.pth')
-    torch.save(B_background, f'results/{id}/background_nmf_dictionary.pth')
+    if mode == 'train':
+        torch.save(B_target, f'results/{id}/{config["dataset"]["target"]}_nmf_dictionary.pth')
+        torch.save(B_background, f'results/{id}/background_nmf_dictionary.pth')
     ## --------------------------------------------------------------------------
 
     ## Save Config --------------------------------------------------------------
-    with open(f'results/{id}/{config["dataset"]["target"]}_config.yaml', 'w') as file:
-        yaml.dump(config, file)
+    if mode == 'train':
+        with open(f'results/{id}/{config["dataset"]["target"]}_config.yaml', 'w') as file:
+            yaml.dump(config, file)
     ## --------------------------------------------------------------------------
 
     ## Test separation ----------------------------------------------------------
-    print("Testing separation...")
-    print(f"B_target.shape: {B_target.shape}, B_background.shape: {B_background.shape}")
+    if mode == 'test':
+        print("Testing separation...")
+        print(f"B_target.shape: {B_target.shape}, B_background.shape: {B_background.shape}")
 
-    scores = test_separation(
-        dataloader  = test_dataloader,
-        B_target    = B_target,
-        B_background= B_background,
-        decode_fn   = lambda x: batch_decode(x, n_fft=config['transform']['n_fft'], hop_length=config['transform']['hop_length'], center=config['transform']['center']),
-        device      = device,
-        n_iter      = config['nmf']['n_iter'],
+        scores = test_separation(
+            dataloader  = test_dataloader,
+            B_target    = B_target,
+            B_background= B_background,
+            decode_fn   = lambda x: batch_decode(x, n_fft=config['transform']['n_fft'], hop_length=config['transform']['hop_length'], center=config['transform']['center']),
+            device      = device,
+            n_iter      = config['nmf']['n_iter'],
         beta        = config['nmf']['beta'],
         alpha       = config['nmf']['alpha'],
-        results_dir = f'results/{id}',
-    )
-    print("Separation tested\n")
-    # print mean scores of each metric
-    for metric in scores.keys():
-        print(f"{metric}: {torch.tensor(scores[metric]).mean().item()}")   
-
+            results_dir = f'results/{id}',
+        )
+        print("Separation tested\n")
+        # print mean scores of each metric
+        for metric in scores.keys():
+            print(f"{metric}: {torch.tensor(scores[metric]).mean().item()}")   
     ## --------------------------------------------------------------------------
 
     ## Save scores --------------------------------------------------------------
-    with open(f'results/{id}/scores.yaml', 'w') as file:
-        yaml.dump(scores, file)
+    if mode == 'test':
+        with open(f'results/{id}/scores.yaml', 'w') as file:
+            yaml.dump(scores, file)
     ## --------------------------------------------------------------------------   
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run NMF source separation with config file')
     parser.add_argument('config_path', type=str, help='Path to config.yaml file')
+    parser.add_argument('--mode', type=str, help='Mode: train or test', default='train')
     args = parser.parse_args()
-    main(args.config_path)
+    main(args.config_path, args.mode)
